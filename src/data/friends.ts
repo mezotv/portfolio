0a84e7d5e68e77094103eeeb66ba73055d7e8a91
Dominik Koch <dominik@koch-bautechnik.de>
2025-06-28 22:55:05 +0200

    feat: add maze as friend

--- src/data/friends.ts
+++ src/data/friends.ts
@@ -19,5 +19,10 @@ export const friendButtons: FriendButton[] = [
     href: "https://panley.みんな/",
     imgSrc: "https://blog.pnly.io/content/images/2025/01/panley.png",
     alt: "panley 88x31 button"
+  },
+  {
+    href: "https://mazecoding.com/",
+    imgSrc: "https://mazecoding.com/images/button.png",
+    alt: "maze 88x31 button"
   }
 ]; 
