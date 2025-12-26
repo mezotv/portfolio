b287593a383fef355130e70e27d49fcc3ea85c1d
Dominik K. <dominik@rivo.gg>
2025-12-26 17:14:00 +0100

    fix: add fonts and initial layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,10 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono, DM_Sans } from "next/font/google";
 import "./globals.css";
+import { LingoProvider } from "@lingo.dev/compiler/react";
+import { ThemeProvider } from "@/components/theme-provider";
 
-const dmSans = DM_Sans({subsets:['latin'],variable:'--font-sans'});
+const dmSans = DM_Sans({ subsets: ['latin'], variable: '--font-sans' });
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,12 +27,21 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className={dmSans.variable}>
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
-    </html>
+    <LingoProvider>
+      <html lang="en" className={dmSans.variable}>
+        <body
+          className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        >
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
+              {children}
+          </ThemeProvider>
+        </body>
+      </html>
+    </LingoProvider>
   );
 }
--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,20 @@
 import type { NextConfig } from "next";
+import { withLingo } from "@lingo.dev/compiler/next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
   reactCompiler: true,
 };
 
-export default nextConfig;
+export default async function (): Promise<NextConfig> {
+  return await withLingo(nextConfig, {
+    sourceRoot: "./app",
+    sourceLocale: "en",
+    targetLocales: ["de"],
+    models: "lingo.dev",
+    dev: {
+      usePseudotranslator: true,
+    },
+  });
+}
+
