6236863dfe45423fa61e6f156a7944005dcfc647
Dominik K. <dominik@rivo.gg>
2025-12-26 19:19:41 +0100

    fix: properly import icons

--- components/mode-toggle.tsx
+++ components/mode-toggle.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import { Moon, Sun } from "lucide-react"
+import { SunIcon, MoonIcon } from "@phosphor-icons/react"
 import { useTheme } from "next-themes"
 
 export function ModeToggle() {
@@ -12,9 +12,9 @@ export function ModeToggle() {
             className="rounded-md w-9 h-9 p-2 hover:bg-zinc-100 dark:hover:bg-zinc-800 focus:outline-none focus:ring-2 focus:ring-zinc-400 dark:focus:ring-zinc-600 focus:ring-offset-2 focus:ring-offset-white dark:focus:ring-offset-black transition-all cursor-pointer flex items-center justify-center group"
         >
             {theme === "dark" ? (
-                <Sun className="h-[1.2rem] w-[1.2rem]" />
+                <SunIcon className="h-[1.2rem] w-[1.2rem]" /> 
             ) : (
-                <Moon className="h-[1.2rem] w-[1.2rem]" />
+                <MoonIcon className="h-[1.2rem] w-[1.2rem]" />
             )}
             <span className="sr-only">Toggle theme</span>
         </button>
