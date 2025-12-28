4d82e6e9e27bd2aef2225a91dad18a79819cc88c
Dominik K. <dominik@rivo.gg>
2025-12-28 21:26:31 +0100

    fix: switch to transparent

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,7 +25,7 @@ export const metadata: Metadata = {
 };
 
 export const viewport: Viewport = {
-  themeColor: "var(--background)",
+  themeColor: "transparent",
 };
 
 export default function RootLayout({
