c854e8acb3fe57b7213c0924b182fac24452306e
Dominik K. <dominik@rivo.gg>
2026-01-05 23:23:05 +0100

    fix: add project to sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -10,6 +10,12 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
     {
       url: BASE_URL,
       lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 1,
+    },
+    {
+      url: `${BASE_URL}/projects`,
+      lastModified: new Date(),
       changeFrequency: "monthly",
       priority: 1,
     },
