423126a7f676be4c792dbc9bc8f881fc75d29f55
Dominik K. <dominik@rivo.gg>
2026-05-27 18:00:44 +0200

    style: use sans font and bold all headings in prose

--- components/prose.tsx
+++ components/prose.tsx
@@ -10,7 +10,7 @@ export function Prose({ children, html, className }: ProseProps) {
   return (
     <article
       className={cn(
-        "prose mx-auto prose-img:rounded-xl prose-p:text-justify prose-h1:font-bold prose-headings:font-normal prose-headings:font-serif prose-a:text-foreground prose-blockquote:text-foreground prose-code:text-foreground prose-em:text-foreground prose-h1:text-xl prose-headings:text-foreground prose-li:text-foreground prose-ol:text-foreground prose-strong:text-foreground prose-ul:text-foreground text-foreground prose-a:underline",
+        "prose mx-auto prose-img:rounded-xl prose-p:text-justify prose-headings:font-bold prose-a:text-foreground prose-blockquote:text-foreground prose-code:text-foreground prose-em:text-foreground prose-h1:text-xl prose-headings:text-foreground prose-li:text-foreground prose-ol:text-foreground prose-strong:text-foreground prose-ul:text-foreground text-foreground prose-a:underline",
         className
       )}
     >
